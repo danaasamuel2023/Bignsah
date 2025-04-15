8c0269d8c723165f45779fe54c2e6e2b676b9e37
danaasamuel2023 <157542114+danaasamuel2023@users.noreply.github.com>
2025-04-15 03:48:57 -1200

    hello

--- client/dataselling/app/telecel/page.js
+++ client/dataselling/app/telecel/page.js
@@ -186,7 +186,7 @@ const TelecelBundleCards = () => {
         userId: userId,
         phoneNumber: trimmedPhoneNumber,
         network: bundle.network,
-        dataAmount: dataAmountInGB,
+        dataAmount: bundle.mb,
         price: parseFloat(bundle.price),
         reference: reference
       }, {
